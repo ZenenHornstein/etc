eb3d6251a71134e60535d24d70b3f534f8c08519
zenenhornstein <zenenhornstein2@gmail.com>
2019-11-07 14:21:17 -0800

    rpi plugun set up

---
+++ vim_plugin_setup_RPI.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#install incosolato go font
+wget -P /usr/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/InconsolataGo/Bold/complete/InconsolataGo%20Bold%20Nerd%20Font%20Complete.ttf
