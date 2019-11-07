6e1a251f514cba959f8038a73e63ac9985f63a4e
zenenhornstein <zenenhornstein2@gmail.com>
2019-11-07 14:22:44 -0800

    download vimplug

--- vim_plugin_setup_RPI.sh
+++ vim_plugin_setup_RPI.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+#Download Vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 #install incosolato go font
 wget -P /usr/share/fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/InconsolataGo/Bold/complete/InconsolataGo%20Bold%20Nerd%20Font%20Complete.ttf
